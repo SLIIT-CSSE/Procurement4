2b3b313718f07fed2db19a573b7be4176f0887d0
HSajinika <43310658+HSajinika@users.noreply.github.com>
2018-10-04 20:53:03 +0530

    Interfaces added for successfullPayments

---
+++ Procurment/Controllers/PaymentsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Procurment.Models;
+
+namespace Procurment.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public PaymentsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: SuccessPayments
+        public ViewResult SuccessfullPaymentList()
+        {
+            var successpayments = _context.Payments.ToList();
+            return View(successpayments);
+        }
+
+        public ActionResult PaidItemDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
